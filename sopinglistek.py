57e47045f209d6604bf0a15d8693a708e90d3738
Igor <podobnik.igor@gmail.com>
2015-10-06 22:06:14 +0200

    test

---
+++ sopinglistek.py
@@ -0,0 +1,25 @@
+__author__ = 'Igor'
+# -*- coding: utf-8 -*-
+
+
+list = []
+i=0
+hoces = raw_input("Zelite kaj vnesti? D za Da: ").lower()
+if hoces == "d":
+    i = 1
+else:
+    print("Pa nic...")
+while i == 1:
+    vnos = raw_input("Vnesi prvo stvar za nakupovalni seznam: ")
+    list.append(vnos)
+    pogoj = raw_input("Ce zelite nadaljevati pritisnite D: ").lower()
+    if pogoj != "d":
+
+        break
+if hoces == "d":
+    print("")
+    print("Vas seznam vsebuje:")
+    for i in list:
+        print i
+    print("--- Konec seznama ---")
+
