da128a0e1c0b9e26b6275d8413041cecf8332878
Igor Podobnik <podobnik.igor@gmail.com>
2015-10-06 16:12:15 +0200

    se en

--- fuzzbizz.py
+++ fuzzbizz.py
@@ -1,17 +1,22 @@
 __author__ = 'Igor'
+
 x = input("Vnesite tevilko od 1 do 100: ")
 if x > 100:
     x = 100
     print("Stevilka prevelika, nastavljena na 100")
-for i in range(1,x,1):
+if x < 1:
+    x = 1
+    print("Stevilka premajhna in nastavljena na 1")
+
+for i in range(1,x + 1 ,1):
     #print i
     #print i % 3
     #print i % 5
-    if ((i % 3) == 0) and ((i % 5) == 0) and i > 0 :
+    if ((i % 3) == 0) and ((i % 5) == 0)  :
         print("fizzbuzz")
-    elif ((i % 3) == 0) and i > 0:
+    elif ((i % 3) == 0):
         print("fizz")
-    elif ((i % 5) == 0) and i > 0:
+    elif ((i % 5) == 0):
         print("buzz")
     else:
         print i
