14a0ad2d3eb2095fe77f91c3fb0748b83e2ba2df
Igor Podobnik <podobnik.igor@gmail.com>
2015-10-07 10:18:23 +0200

    se en

--- uganiprestolnico.py
+++ uganiprestolnico.py
@@ -1,7 +1,16 @@
 __author__ = 'igorpodobnik'
+import random
 prestolnice = {
 "Slovenija":"Ljubljana",
 "Hrvaska" : "Zagreb"
 }
-
+random.choice(prestolnice.keys())
 print prestolnice
+
+
+#random.sample()
+#random.choice()
+#dist.keys()
+#dist.value()
+random_key = random.sample(prestolnice, 1)[0]
+print random_key
