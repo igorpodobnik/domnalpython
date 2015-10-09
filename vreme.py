7978d180aae0205142414c209257c1ba40493d57
Igor Podobnik <podobnik.igor@gmail.com>
2015-10-09 16:48:59 +0200

    se en

--- vreme.py
+++ vreme.py
@@ -1,10 +1,23 @@
-__author__ = 'igorpodobnik'
-#!/usr/bin/python
-
 import urllib2
-response = urllib2.urlopen('http://python.org/')
-html = response.read()
+from bs4 import BeautifulSoup
 
-#print html
+# not done yet!!!!
 
-# NOT DONE YET
+soup = BeautifulSoup(urllib2.urlopen("http://www.arso.gov.si/vreme/napovedi%20in%20podatki/vreme_si_w.html").read(), "lxml")
+#soup = BeautifulSoup(urllib2.urlopen("http://pro-vreme.net/index.php?id=8").read(), "lxml")
+#result = soup.select("#glavna" > "tbody" > "tr" > "td[2]"> "table")
+#print result
+
+
+for p in divs.select('.//*[@id="glavna"]/tbody/tr/td[2]/table'):
+# extracts all <p> inside
+    print p.extract()
+
+
+for row in soup("table", {"id": "glavna"})[0].tbody('tr'):
+    tds = row('td')
+    print tds[0].string, tds[1].string
+    #//*[@id="glavna"]/tbody/tr/td[2]/table
+    #//*[@id="glavna"]/tbody/tr/td[2]/table
+
+    #father.findNext('div',{'class':'class_value'}).findNext('div',{'id':'id_value'}).findAll('a')
