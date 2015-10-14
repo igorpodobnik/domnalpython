1d62ed73ba560915bc066285eae895f04a09bdc2
Igor Podobnik <podobnik.igor@gmail.com>
2015-10-14 12:17:35 +0200

    se en

--- sopinglistek.py
+++ sopinglistek.py
@@ -2,22 +2,22 @@ __author__ = 'Igor'
 # -*- coding: utf-8 -*-
 import os
 
-list = []
+soping_list = []
 i=0
 hoces = raw_input("Zelite kaj vnesti? D za Da: ").lower()
 if hoces != "d":
     print("Pa nic...")
 while hoces == "d":
-    vnos = raw_input("Vnesi prvo stvar za nakupovalni seznam: ")
-    list.append(vnos)
-    pogoj = raw_input("Ce zelite nadaljevati pritisnite D: ").lower()
-    if pogoj != "d":
+    vnos = raw_input("Vnesi novo stvar za nakupovalni seznam (oz. p za prekinitev): ")
 
+    #pogoj = raw_input("Ce zelite nadaljevati pritisnite D: ").lower()
+    if vnos == "p":
         break
+    soping_list.append(vnos)
 if hoces == "d":
     print "\n" * 20
     print("Vas seznam vsebuje:")
-    for i in list:
+    for i in soping_list:
         print i
     print("--- Konec seznama ---")
 
