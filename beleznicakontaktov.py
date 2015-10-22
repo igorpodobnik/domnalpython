3cf794ca3465bdc6461b94da3edcc898a06d2e3a
Igor Podobnik <podobnik.igor@gmail.com>
2015-10-22 16:00:48 +0200

    start

--- beleznicakontaktov.py
+++ beleznicakontaktov.py
@@ -17,22 +17,29 @@ class kontakt():
 
 def main():
     pogoj = "da"
-    while pogoj == "da":
+    Imenik = []
+    for i in range(0,255):
         pogoj=raw_input("Vpisite Da za vnos v koledar: ").lower()
         print pogoj
+        print i
         if pogoj != "da":
             break
-        else:
-            print "dal si DA"
+        #else:
+            #print "dal si DA"
+        ime=raw_input("Vpisi ime: ")
+        print ime
+        Imenik.append(kontakt(ime,"0","0","0","0"))
 
-    prvi = kontakt("Igor","Podobnik","igor@igor.si","0044","Chengdujska")
+"""    prvi = kontakt("Igor","Podobnik","igor@igor.si","0044","Chengdujska")
     print "haha"
     print prvi
     print prvi.first_name
     print prvi.last_name
     print ("%s in %s" % (prvi.first_name,prvi.last_name))
-    print "end"
-
+    print "end"     """
+    #print Imenik
+    for i in Imenik:
+        print i.first_name
 
 
 if __name__ == "__main__":
