fa08d9dfb985ea5dee8c77b87091b3cd13a9a6a5
Igor <podobnik.igor@gmail.com>
2015-10-22 20:11:19 +0200

    malo bolje

--- vreme.py
+++ vreme.py
@@ -1,23 +1,11 @@
-import urllib2
+
 from bs4 import BeautifulSoup
 
-# not done yet!!!!
-
-soup = BeautifulSoup(urllib2.urlopen("http://www.arso.gov.si/vreme/napovedi%20in%20podatki/vreme_si_w.html").read(), "lxml")
-#soup = BeautifulSoup(urllib2.urlopen("http://pro-vreme.net/index.php?id=8").read(), "lxml")
-#result = soup.select("#glavna" > "tbody" > "tr" > "td[2]"> "table")
-#print result
 
 
-for p in divs.select('.//*[@id="glavna"]/tbody/tr/td[2]/table'):
-# extracts all <p> inside
-    print p.extract()
+url_str = 'http://www.pro-vreme.net/index.php?id=8'
+import requests
 
+r = requests.get(url_str).content
 
-for row in soup("table", {"id": "glavna"})[0].tbody('tr'):
-    tds = row('td')
-    print tds[0].string, tds[1].string
-    #//*[@id="glavna"]/tbody/tr/td[2]/table
-    #//*[@id="glavna"]/tbody/tr/td[2]/table
-
-    #father.findNext('div',{'class':'class_value'}).findNext('div',{'id':'id_value'}).findAll('a')
+print([x.text for x in BeautifulSoup(r).find_all("table",attrs={"allign":"center"})])
