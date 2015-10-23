e29ee57c3ad09f9570f074f80f34676fd49cc47e
Igor Podobnik <podobnik.igor@gmail.com>
2015-10-23 18:33:42 +0200

    Igra ugani avto, vneseni avtomobili, dodan random.

--- uganiavto.py
+++ uganiavto.py
@@ -23,12 +23,25 @@ class avtomobili():
     def __repr__(self):
         return "STR %d %s %s %s %s %s " % (self.zap_st,self.znamka,self.st_vrat,self.model,self.pogon,self.tip_motorja)
 
+def koncaj(pogoj,kontr):
+    if pogoj == kontr:
+        print "Yeeeey"
+        return False
+    else:
+        return True
+
+
 
 avti = []
 avti.append(avtomobili("kia",0,"5","Carens","Spredaj","Diesel"))
 avti.append(avtomobili("renault",1,"5","Clio","Spredaj","Bencin"))
 avti.append(avtomobili("porshe",2,"2","911","Zadaj","Bencin"))
+avti.append(avtomobili("bmw",3,"4","3 Series","Zadaj","Diesel"))
 
+
+
+
+loop = True
 #print(cifra)
 
 #print avti
@@ -38,36 +51,35 @@ while True:
     if se != "d":
         break
     cifra = random.randrange(len(avti))
-    #print cifra
+    #print str(cifra)+"cifra"
+    loop = True
     for i in avti:
-        #print i.zap_st
+        #print str(i.zap_st) + "zap"
         if cifra == i.zap_st:
-            while True:
+            while loop == True:
                 #print "hello " + str(i.model)
                 kater_podatek = random.randrange(3)
-                #print(kater_podatek)
+                #print str(kater_podatek) + "kater_podatek"
                 if kater_podatek==0:
                     #print i.st_vrat
                     print ("Stevilo vrat je %s: " % i.st_vrat)
                     uganka = raw_input("Vnesi znamko: ").lower()
-                    if uganka == i.znamka:
-                        print "YEY"
-                        break
+                    loop = koncaj(uganka,i.znamka)
+                    #print str(loop) + "loop"
+                    #if uganka == i.znamka:
+                        #print "YEY"
+                        #break
 
                 elif kater_podatek == 1:
                     #print i.model
                     print ("Model je %s: " % i.model)
                     uganka = raw_input("Vnesi znamko: ").lower()
-                    if uganka == i.znamka:
-                        print "YEY"
-                        break
+                    loop = koncaj(uganka,i.znamka)
 
                 elif kater_podatek == 2:
                     #print i.model
                     print ("Pogon ima %s: " % i.pogon)
                     uganka = raw_input("Vnesi znamko: ").lower()
-                    if uganka == i.znamka:
-                        print "YEY"
-                        break
+                    loop = koncaj(uganka,i.znamka)
 
 
